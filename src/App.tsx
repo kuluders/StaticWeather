f20eefc2145fdd0bd6c92b5e2fb6121cff7a4f0a
kuluders <66642915+kuluders@users.noreply.github.com>
2023-08-14 15:45:54 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Title from './Components/Title'
+import Weather from './Components/Weather'
+
+export default function App() {
+  return (
+    <div>
+      <Title />
+      <Weather />
+    </div>
+  )
+}
